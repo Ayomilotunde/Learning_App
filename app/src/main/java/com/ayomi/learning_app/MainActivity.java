6c4e68fcb731ee7f33cf3cea709f1c3d6ef8deeb
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-20 16:23:50 +0100

    update

--- app/src/main/java/com/ayomi/learning_app/MainActivity.java
+++ app/src/main/java/com/ayomi/learning_app/MainActivity.java
@@ -1,11 +1,13 @@
 package com.ayomi.learning_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void sendMessage(View view) {
