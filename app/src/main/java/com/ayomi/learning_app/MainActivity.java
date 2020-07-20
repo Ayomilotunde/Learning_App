7cee2271a2c58022080f81680918a0ba7686d8f5
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-20 16:36:54 +0100

    update

--- app/src/main/java/com/ayomi/learning_app/MainActivity.java
+++ app/src/main/java/com/ayomi/learning_app/MainActivity.java
@@ -1,12 +1,17 @@
 package com.ayomi.learning_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MessageActivity.class);
         intent.putExtra("message", message);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add:
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove:
+                Toast.makeText(this, "Remove", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.quit:
+                Toast.makeText(this, "Quit", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
